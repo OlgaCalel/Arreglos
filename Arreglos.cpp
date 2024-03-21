096dce65c48f8254f42bdc223346a9ea0feb32e6
OlgaCalel <161219868+OlgaCalel@users.noreply.github.com>
2024-03-20 22:44:56 -0600

    Update Arreglos.cpp

--- Arreglos.cpp
+++ Arreglos.cpp
@@ -1,1 +1,31 @@
-Codigos
+
+#include <iostream>
+using namespace std;
+
+main(){
+
+
+int fil=0, col=0;
+cout<<"Ingrese filas: ";
+cin>>fil;
+cout<<"Ingrese columnas: ";
+cin>>col;
+int matriz[fil][col];
+
+cout<<"---------------Ingresar---------------"<<endl;
+for (int i=0;i<fil;i++){
+	for(int ii=0; ii<col;ii++){
+		cout<<i<<","<<ii<<": ";
+		cin>>matriz[i][ii];
+	}
+	cout<<"°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°"<<endl;
+}
+cout<<"---------------Mostrar---------------"<<endl;
+for (int i=0;i<fil;i++){
+	for(int ii=0; ii<col;ii++){
+		cout<<i<<","<<ii<<": "<<matriz[i][ii]<<endl;
+	}
+}
+
+	return 0;
+}
